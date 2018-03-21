8e9fe6a9d1286f7aa0836e1b67964f0c09656178
“michal-adamczyk8 <adamczyk888@gmail.com>
2018-03-21 21:17:16 +0100

    Zadanie 13.3

--- src/main/java/com/kodilla/hibernate/invoice/Item.java
+++ src/main/java/com/kodilla/hibernate/invoice/Item.java
@@ -33,9 +33,8 @@ public class Item {
         return id;
     }
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "PRODUCT_ID")
-    @Column(name = "PRODUCT")
     public Product getProduct() {
         return product;
     }
@@ -75,7 +74,7 @@ public class Item {
         this.value = value;
     }
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "INVOICE_ID")
     public Invoice getInvoice() {
         return invoice;
