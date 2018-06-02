fb5509b53b499b2df656df4d7746a0d2b45904ba
Michał Adamczyk <adamczyk888@gmail.com>
2018-06-02 13:27:51 +0200

    Pprawione zadanie 20.1

--- src/test/java/com/kodilla/hibernate/manytomany/facade/CompanyFacadeTestSuite.java
+++ src/test/java/com/kodilla/hibernate/manytomany/facade/CompanyFacadeTestSuite.java
@@ -26,32 +26,79 @@ public class CompanyFacadeTestSuite {
     private CompanyFacade companyFacade;
 
     @Test
-    public void testCompanyFacade() {
-        //Given
+    public void shouldRetrieveEmployeeWithPieceOfName() {
         Employee employee1 = new Employee("Krzysztof", "Olejniczak");
         Employee employee2 = new Employee("Jan", "Kowalski");
         Employee employee3 = new Employee("Tony", "Soprano");
-        Company company1 = new Company("Facebook");
-        Company company2 = new Company("Mercer");
-        Company company3 = new Company("Adidas");
         employeeDao.save(employee1);
         employeeDao.save(employee2);
         employeeDao.save(employee3);
+
+        //When
+        List<Employee> retrievedEmployees = companyFacade.lookForEmployees("lejnicz");
+        String nameOfRetrievedEmployee = retrievedEmployees.get(0).getLastName();
+
+        //Then
+        Assert.assertEquals(1, retrievedEmployees.size());
+        Assert.assertEquals("Olejniczak", nameOfRetrievedEmployee);
+
+        //Clean Up
+        employeeDao.delete(employee1);
+    }
+
+    @Test
+    public void shouldRetrieveCompaniesWithNameLike() {
+        //Given
+        Company company1 = new Company("Facebook");
+        Company company2 = new Company("Mercer");
+        Company company3 = new Company("Adidas");
         companyDao.save(company1);
         companyDao.save(company2);
         companyDao.save(company3);
+
         //When
-        List<Employee> retrievedEmployees = companyFacade.lookForEmployees("lejnicz");
         List<Company> retrievedCompanies = companyFacade.lookForCompanies("dida");
-        String nameOfRetrievedEmployee = retrievedEmployees.get(0).getLastName();
         String nameOfRetrievedCompany = retrievedCompanies.get(0).getName();
 
         //Then
         Assert.assertEquals(1, retrievedCompanies.size());
-        Assert.assertEquals("Olejniczak", nameOfRetrievedEmployee);
-        Assert.assertEquals(1, retrievedEmployees.size());
         Assert.assertEquals("Adidas", nameOfRetrievedCompany);
-        employeeDao.delete(employee1);
+
+        //Clean Up
         companyDao.delete(company3);
+    }
+
+    @Test
+    public void shouldRetrieveNoEmployees() {
+        //Given
+        Employee employee1 = new Employee("Krzysztof", "Wawrzyniak");
+        Employee employee2 = new Employee("Jan", "Kowalski");
+        Employee employee3 = new Employee("Tony", "Soprano");
+        employeeDao.save(employee1);
+        employeeDao.save(employee2);
+        employeeDao.save(employee3);
+
+        //When
+        List<Employee> retrievedEmployees = companyFacade.lookForEmployees("lejnicz");
+
+        //Then
+        Assert.assertNotEquals(1, retrievedEmployees.size());
+    }
+
+    @Test
+    public void shouldRetrieveNoCompanies() {
+        //Given
+        Company company1 = new Company("Facebook");
+        Company company2 = new Company("Mercer");
+        Company company3 = new Company("Nike");
+        companyDao.save(company1);
+        companyDao.save(company2);
+        companyDao.save(company3);
+
+        //When
+        List<Company> retrievedCompanies = companyFacade.lookForCompanies("dida");
+
+        //Then
+        Assert.assertNotEquals(1, retrievedCompanies.size());
     }
 }
